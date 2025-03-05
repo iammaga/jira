23f904b37191af1419796c0b4ba7f51c4e3d3b7e
iammaga <muxammad.zikirzoda@gmail.com>
2025-03-05 07:47:22 +0500

    deleted vite

--- resources/views/vendor/laratrust/panel/layout.blade.php
+++ resources/views/vendor/laratrust/panel/layout.blade.php
@@ -13,53 +13,70 @@
 </head>
 <body>
 <div>
-    <nav class="bg-gray-800">
+    <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+        <!-- Primary Navigation Menu -->
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div class="flex items-center justify-center h-16">
-                <div class="flex items-center">
-                    <div class="hidden md:block">
-                        <div class="flex items-baseline space-x-4">
-                            <a href="{{ config('laratrust.panel.go_back_route') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">← Go Back</a>
-                            <a href="{{ route('laratrust.roles-assignment.index') }}"
-                               class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium {{ request()->is('*roles-assignment*') ? 'bg-gray-900 text-white' : '' }}">
-                                Roles & Permissions Assignment
-                            </a>
-                            <a href="{{ route('laratrust.roles.index') }}"
-                               class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium {{ request()->is('*roles') ? 'bg-gray-900 text-white' : '' }}">
-                                Roles
-                            </a>
-                            <a href="{{ route('laratrust.permissions.index') }}"
-                               class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium {{ request()->is('*permissions*') ? 'bg-gray-900 text-white' : '' }}">
-                                Permissions
-                            </a>
-                        </div>
-                    </div>
-                    <div class="ml-10 flex-shrink-0">
-                        <!-- SVG логотип остаётся без изменений -->
-                        <svg class="h-10 w-auto" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns="http://www.w3.org/2000/svg" height="80" width="272" version="1.1" viewBox="0 0 272 80">
-                            <!-- Весь SVG код остаётся -->
-                        </svg>
+            <div class="flex justify-between h-16">
+                <div class="flex">
+                    <!-- Logo -->
+{{--                    <div class="shrink-0 flex items-center">--}}
+{{--                        <svg class="h-10 w-auto" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns="http://www.w3.org/2000/svg" height="80" width="272" version="1.1" viewBox="0 0 272 80">--}}
+{{--                            <!-- Весь SVG код остаётся без изменений -->--}}
+{{--                        </svg>--}}
+{{--                    </div>--}}
+
+                    <!-- Navigation Links -->
+                    <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                        <a href="{{ route('dashboard') }}"
+                           class="inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium leading-5 transition duration-150 ease-in-out {{ request()->is('/') ? 'border-indigo-400 text-gray-900 focus:border-indigo-700' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300' }}">
+                            ← Dashboard
+                        </a>
+                        <a href="{{ route('laratrust.roles-assignment.index') }}"
+                           class="inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium leading-5 transition duration-150 ease-in-out {{ request()->is('*roles-assignment*') ? 'border-indigo-400 text-gray-900 focus:border-indigo-700' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300' }}">
+                            Roles & Permissions Assignment
+                        </a>
+                        <a href="{{ route('laratrust.roles.index') }}"
+                           class="inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium leading-5 transition duration-150 ease-in-out {{ request()->is('*roles') ? 'border-indigo-400 text-gray-900 focus:border-indigo-700' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300' }}">
+                            Roles
+                        </a>
+                        <a href="{{ route('laratrust.permissions.index') }}"
+                           class="inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium leading-5 transition duration-150 ease-in-out {{ request()->is('*permissions*') ? 'border-indigo-400 text-gray-900 focus:border-indigo-700' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300' }}">
+                            Permissions
+                        </a>
                     </div>
                 </div>
-                <div class="-mr-2 flex md:hidden">
-                    <button class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white">
-                        <svg class="block h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        </svg>
-                        <svg class="hidden h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+
+                <!-- Hamburger -->
+                <div class="-me-2 flex items-center sm:hidden">
+                    <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                        <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                            <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                            <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                         </svg>
                     </button>
                 </div>
             </div>
         </div>
-        <div class="hidden md:hidden">
-            <div class="px-2 pt-2 pb-3 sm:px-3">
-                <a href="/" class="block px-3 py-2 rounded-md text-base font-medium text-white bg-gray-900">Dashboard</a>
-                <a href="#" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700">Team</a>
-                <a href="#" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700">Projects</a>
-                <a href="#" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700">Calendar</a>
-                <a href="#" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700">Reports</a>
+
+        <!-- Responsive Navigation Menu -->
+        <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+            <div class="pt-2 pb-3 space-y-1">
+                <a href="{{ config('laratrust.panel.go_back_route') }}"
+                   class="block px-3 py-2 rounded-md text-base font-medium {{ request()->is('/') ? 'bg-gray-900 text-white' : 'text-gray-700 hover:bg-gray-700 hover:text-white' }}">
+                    ← Go Back
+                </a>
+                <a href="{{ route('laratrust.roles-assignment.index') }}"
+                   class="block px-3 py-2 rounded-md text-base font-medium {{ request()->is('*roles-assignment*') ? 'bg-gray-900 text-white' : 'text-gray-700 hover:bg-gray-700 hover:text-white' }}">
+                    Roles & Permissions Assignment
+                </a>
+                <a href="{{ route('laratrust.roles.index') }}"
+                   class="block px-3 py-2 rounded-md text-base font-medium {{ request()->is('*roles') ? 'bg-gray-900 text-white' : 'text-gray-700 hover:bg-gray-700 hover:text-white' }}">
+                    Roles
+                </a>
+                <a href="{{ route('laratrust.permissions.index') }}"
+                   class="block px-3 py-2 rounded-md text-base font-medium {{ request()->is('*permissions*') ? 'bg-gray-900 text-white' : 'text-gray-700 hover:bg-gray-700 hover:text-white' }}">
+                    Permissions
+                </a>
             </div>
         </div>
     </nav>
