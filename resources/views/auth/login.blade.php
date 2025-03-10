6888864745a88ee2b391a02776e8aac95104c97e
iammaga <muxammad.zikirzoda@gmail.com>
2025-03-10 18:23:19 +0500

    Fixed view bugs register & login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,14 +32,14 @@
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="mt-4 text-right">
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                <a class="block underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
 
-            <x-primary-button class="ms-3">
+            <x-primary-button class="mt-4">
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
